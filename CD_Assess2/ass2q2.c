67dd47c02a4f635b90a21108dc9aa7427886ac76
Krish Tejwani <krikill.424@gmail.com>
2024-09-02 22:10:21 +0530

    assignment 2

--- CD_Assess2/ass2q2.c
+++ CD_Assess2/ass2q2.c
@@ -5,6 +5,47 @@
 #define MAX_TOKEN_LENGTH 100
 #define MAX_LINE_LENGTH 256
 
+// List of keywords
+const char *keywords[] = {"int", "return", "if", "else", "while", "for", "do", "switch", "case", "default", "break", "continue", "void", "char", "float", "double", "long", "short", "unsigned", "signed", "static", "struct", "union", "enum", "typedef", "const", "sizeof", "volatile", "extern", "register", "auto", "goto", "include", "define", NULL};
+
+// Function to check if a token is a keyword
+int isKeyword(const char *token) {
+    for (int i = 0; keywords[i] != NULL; i++) {
+        if (strcmp(token, keywords[i]) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Function to print names with their symbols
+void printOperatorName(char c) {
+    printf("Operator, ");
+    switch (c) {
+        case '+': printf("Addition: %c\n\n", c); break;
+        case '-': printf("Subtraction: %c\n\n", c); break;
+        case '*': printf("Multiplication: %c\n\n", c); break;
+        case '/': printf("Division: %c\n\n", c); break;
+        case '=': printf("Assignment: %c\n\n", c); break;
+        case '<': printf("Less than: %c\n\n", c); break;
+        case '>': printf("Greater than: %c\n\n", c); break;
+        case '&': printf("Logical AND: %c\n\n", c); break;
+        case '|': printf("Logical OR: %c\n\n", c); break;
+        case '!': printf("Logical NOT: %c\n\n", c); break;
+        case '^': printf("Bitwise XOR: %c\n\n", c); break;
+        case '%': printf("Modulus: %c\n\n", c); break;
+        case '(': printf("Left parenthesis: %c\n\n", c); break;
+        case ')': printf("Right parenthesis: %c\n\n", c); break;
+        case '{': printf("Left curly brace: %c\n\n", c); break;
+        case '}': printf("Right curly brace: %c\n\n", c); break;
+        case '[': printf("Left square bracket: %c\n\n", c); break;
+        case ']': printf("Right square bracket: %c\n\n", c); break;
+        case ',': printf("Comma: %c\n\n", c); break;
+        case ';': printf("Semicolon: %c\n\n", c); break;
+        default: break;
+    }
+}
+
 void identifyTokens(char *input) {
     char token[MAX_TOKEN_LENGTH];
     int i = 0, j = 0;
@@ -16,13 +57,18 @@ void identifyTokens(char *input) {
             continue;
         }
 
-        // If the character is an alphabet or underscore (part of an identifier)
+        // If the character is an alphabet or underscore (part of an identifier or keyword)
         if (isalpha(input[i]) || input[i] == '_') {
             while (isalpha(input[i]) || isdigit(input[i]) || input[i] == '_') {
                 token[j++] = input[i++];
             }
             token[j] = '\0';
-            printf("Identifier: %s\n", token);
+
+            if (isKeyword(token)) {
+                printf("Keyword: %s\n\n", token);
+            } else {
+                printf("Identifier: %s\n\n", token);
+            }
             j = 0;
         }
 
@@ -32,27 +78,34 @@ void identifyTokens(char *input) {
                 token[j++] = input[i++];
             }
             token[j] = '\0';
-            printf("Number: %s\n", token);
+            printf("Number: %s\n\n", token);
             j = 0;
         }
 
-        // If the character is an operator
-        else if (strchr("+-*/=", input[i])) {
+        // If the character is an
+        else if (strchr("+-*/=<>!&|^%", input[i])) {
+            token[j++] = input[i++];
+            // Check for multi-characters like '==', '!=', '<=', '>='
+            if ((token[0] == '=' || token[0] == '!' || token[0] == '<' || token[0] == '>') && input[i] == '=') {
+                token[j++] = input[i++];
+            } else if ((token[0] == '&' || token[0] == '|') && input[i] == token[0]) {
+                token[j++] = input[i++];
+            }
+            token[j] = '\0';
+            printf("Operator: %s\n\n", token);
+            printOperatorName(token[0]);
+            j = 0;
+        }
+
+        // If the character is a special symbol (like ;, (), {}, etc.)
+        else if (strchr("(){}[];,", input[i])) {
             token[j++] = input[i++];
             token[j] = '\0';
-            printf("Operator: %s\n", token);
+            printOperatorName(token[0]);
             j = 0;
         }
 
-        // If the character is a special symbol (like ;, (), etc.)
-        else if (strchr(";(),{}", input[i])) {
-            token[j++] = input[i++];
-            token[j] = '\0';
-            printf("Special Symbol: %s\n", token);
-            j = 0;
-        }
-
-        // Move to next character
+        // For unrecognized characters, just skip them
         else {
             i++;
         }
@@ -69,7 +122,7 @@ void identifyTokensFromFile(FILE *file) {
 int main() {
     FILE *file = fopen("input_program.txt", "r");
     if (!file) {
-        printf("Error: Could not open file.\n");
+        printf("Error: Could not open file.\n\n");
         return 1;
     }
 
